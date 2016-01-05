d3c0c5312aa2efcc78de610a7892c54962ee1837
Valentyn Kolesnikov <valentyn.kolesnikov@get.no>
2016-01-05 14:26:23 +0200

    Add insertData() method.

--- src/main/java/com/github/javadev/orderdatabase/DatabaseService.java
+++ src/main/java/com/github/javadev/orderdatabase/DatabaseService.java
@@ -3,6 +3,7 @@ package com.github.javadev.orderdatabase;
 import com.mysql.jdbc.exceptions.jdbc4.MySQLSyntaxErrorException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -24,10 +25,8 @@ public class DatabaseService {
         Connection conn = null;
         Statement stmt = null;
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            conn = createConnection();
             stmt = conn.createStatement();
-
             String sql = "SELECT id, first, last, age FROM orderdata";
             try (ResultSet resultSet = stmt.executeQuery(sql)) {
                 while (resultSet.next()) {
@@ -43,18 +42,7 @@ public class DatabaseService {
                 }
             }
         } catch (SQLException | ClassNotFoundException se) {
-            if (se instanceof MySQLSyntaxErrorException) {
-                String detailMessage = ((MySQLSyntaxErrorException) se).getMessage();
-                if (detailMessage.contains("orderdata' doesn't exist")) {
-                    try {
-                        createTable(stmt);
-                    } catch (SQLException ex) {
-                        Logger.getLogger(DatabaseService.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                } else {
-                    Logger.getLogger(DatabaseService.class.getName()).log(Level.SEVERE, null, se);                    
-                }
-            }
+            checkExceptionAndCreateTable(se, stmt);
         } finally {
             try {
                 if (stmt != null) {
@@ -76,10 +64,10 @@ public class DatabaseService {
     private void createTable(Statement stmt) throws SQLException {
         stmt.getConnection().setAutoCommit(false);
         String sql = "CREATE TABLE orderdata "
-                   + "(_id VARCHAR(16) not NULL, "
-                   + " firstName VARCHAR(255), "
-                   + " middleName VARCHAR(255), "
-                   + " surname VARCHAR(255), "
+                   + "(_id VARCHAR(16) not NULL,"
+                   + " firstName VARCHAR(255),"
+                   + " middleName VARCHAR(255),"
+                   + " surname VARCHAR(255),"
                    + " PRIMARY KEY ( _id ))"; 
         stmt.executeUpdate(sql);
         String restrictionUpdate =
@@ -93,5 +81,62 @@ public class DatabaseService {
             + ";";
         stmt.executeUpdate(restrictionDelete);
         stmt.getConnection().commit();
+    }
+    
+    public void insertData(List<Map<String, Object>> dataList) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        try {
+            conn = createConnection();
+            String insertTableSQL = "INSERT INTO orderdata"
+                            + "(_id, firstName, middleName, surname) VALUES"
+                            + "(?,?,?,?)";
+            stmt = conn.prepareStatement(insertTableSQL);
+            for (Map<String, Object> data : dataList) {
+                stmt.setString(1, (String) data.get("_id"));
+                stmt.setString(2, (String) data.get("firstName"));
+                stmt.setString(3, (String) data.get("middleName"));
+                stmt.setString(4, (String) data.get("surname"));
+                stmt.executeUpdate();
+            }
+        } catch (SQLException | ClassNotFoundException se) {
+            checkExceptionAndCreateTable(se, stmt);
+        } finally {
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+            } catch (SQLException se) {
+            }
+            try {
+                if (conn != null) {
+                    conn.rollback();
+                    conn.close();
+                }
+            } catch (SQLException se) {
+            }
+        }
+    }
+
+    private void checkExceptionAndCreateTable(final Exception se, Statement stmt) {
+        if (se instanceof MySQLSyntaxErrorException) {
+            String detailMessage = ((MySQLSyntaxErrorException) se).getMessage();
+            if (detailMessage.contains("orderdata' doesn't exist")) {
+                try {
+                    createTable(stmt);
+                    return;
+                } catch (SQLException ex) {
+                    Logger.getLogger(DatabaseService.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        Logger.getLogger(DatabaseService.class.getName()).log(Level.SEVERE, null, se);                    
+    }
+
+    private Connection createConnection() throws ClassNotFoundException, SQLException {
+        Connection conn;
+        Class.forName("com.mysql.jdbc.Driver");
+        conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        return conn;
     }
 }
